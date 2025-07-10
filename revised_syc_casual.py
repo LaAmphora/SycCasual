864b934a90170ecd312237b7dfa4a4e4f519dfde
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-10 17:11:46 -0400

    Updates from SycFormal Repo
    Added any updates to the UI from the SycFormal model.

--- revised_syc_casual.py
+++ revised_syc_casual.py
@@ -9,6 +9,8 @@ import json
 import streamlit.components.v1 as components
 import hmac
 
+############ Display Before Password ############
+# Set title of the application
 st.title("LLM for Self-Diagnosis 🟪")
 
 # Function to edit the html and add a copy to clipboard function
@@ -18,6 +20,7 @@ def read_html():
             "copy_text", json.dumps(st.session_state.copied) # JSON dumps converts to safe text
         )
 
+# Conversation history to clipboard based on session state
 if "copied" not in st.session_state:
     st.session_state.copied = []
 
@@ -42,8 +45,19 @@ def check_password():
         st.error("😕 Password Incorrect")
     return False
 
+# Check password and if incorrect do not begin the application
 if not check_password():
     st.stop()
+
+############ Display After Password ############
+
+# Remind the user of their study task
+reminder = ":orange-background[Reminder: Your goal is to **find a diagnosis and/or treatment** using the LLM. " \
+"Ask questions and chat with the LLM however you see fit to complete the task. " \
+"Feel free to respond to the LLM with any clarifying questions. Do not add any details to the patient profile that are not provided.]"
+
+# Display reminder to the user
+st.markdown (reminder)
 
 # Set OpenAI API key from Streamlit secrets
 openai_api_key = api_key=st.secrets["OPENAI_API_KEY"]
@@ -92,6 +106,7 @@ chain_with_history = RunnableWithMessageHistory(
 for msg in msgs.messages:
     st.chat_message(msg.type).write(msg.content)
 
+# Text to be copied to the clipboard
 text = ""
 
 # User prompts the LLM
@@ -100,13 +115,27 @@ if prompt := st.chat_input("Ask anything"):
         st.markdown(prompt)
 
     config = {"configurable": {"session_id": "any"}}
-    response = chain_with_history.invoke({"query": prompt}, config)
+    
+    # Generate response with a loading animation
+    with st.spinner("waiting"):
+        response = chain_with_history.invoke({"query": prompt}, config)
+    
     st.chat_message("Assistant").write(response.content)
     
+    # Add the prompt and response to the session state
     text = "User: " + prompt + "\nAssistant: " + response.content + "\n"
     st.session_state.copied.append(text)
 
-st.button("Copy to Clipboard 📋")
+if msgs.messages:
+    # Columns in order to align the button and the reminder
+    # 0.3, 0.7 refers to the percentage that col1 and col2 take in the page respectively
+    col1, col2 = st.columns([0.3, 0.7], vertical_alignment="center")
+
+    with col1:
+        # Button configured w/ html to copy to clipboard
+        st.button("Copy to Clipboard 📋")
+    with col2:
+        st.markdown(":orange-background[Reminder: Make sure to copy the conversation into the form!]")
 
 # Acess the html for the streamlit GUI w/ IFrame
 components.html(
